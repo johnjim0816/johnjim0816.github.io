ef593835b12ad5d97059a2989ad6067706afda03
johnjim0816 <johnjim0816@gmail.com>
2022-08-08 16:39:51 +0800

    update

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+    echo "当前文件夹已被污染，请在master分支中提交所有commits之后再来执行该文件"
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Push to origin"
+git push origin gh-pages
