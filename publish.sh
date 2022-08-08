b0b2c1f1f883f2ccc0ffc3ff46181484d160856e
johnjim0816 <johnjim0816@gmail.com>
2022-08-08 14:02:23 +0800

    update

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ mkdir public
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B dev public origin/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -21,7 +21,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to github-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Push to origin"
-git push origin dev
+git push origin gh-pages
