659c23e9757239d833145b38c65a45a1c5a892d9
johnjim0816 <johnjim0816@gmail.com>
2022-08-08 13:59:46 +0800

    update

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ mkdir public
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -B dev public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
